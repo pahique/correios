93ae368c2a032acefe5b4591bc4c60d747ce7edb
Paulo Oliveros <V8T2Z6L@johndeere.com>
2021-04-13 23:18:26 -0300

    Troca de nome da aplicação

--- src/main/java/br/com/apicaller/correios/CorreiosApplication.java
+++ src/main/java/br/com/apicaller/correios/CorreiosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalcprecoprazoApplication {
+public class CorreiosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalcprecoprazoApplication.class, args);
+		SpringApplication.run(CorreiosApplication.class, args);
 	}
 
 }
